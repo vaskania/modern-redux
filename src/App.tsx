58bdfefe77cb56bb558183128d47b481f42e6509
vaska <vaskania26@gmail.com>
2022-06-18 09:29:25 +0400

    init modern redux/toolkit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import {useAppSelector, useAppDispatch} from './app/hooks'
+import {incremented, amountAdded} from './features/counter/counter-slice'
+import {useFetchBreedsQuery} from './features/dogs/dogs-api-slice'
+import {useState} from 'react'
+
+function App() {
+    const count = useAppSelector(state => state.counter.value)
+    const dispatch = useAppDispatch()
+    const [numDogs, setNumDogs] = useState(10)
+
+    const {data = [], isFetching} = useFetchBreedsQuery(numDogs)
+
+    const handleClick = () => {
+        dispatch(amountAdded(3))
+    }
+
+    return (
+        <div className="App">
+            <button onClick={handleClick}>increment</button>
+            {count}
+
+            <div>
+                <p>Dogs to fetch</p>
+                <select value={numDogs} onChange={(e) => setNumDogs(Number(e.target.value))}>
+                    <option value='5'>5</option>
+                    <option value='10'>10</option>
+                    <option value='15'>15</option>
+                    <option value='20'>20</option>
+                </select>
+            </div>
+
+            <div>
+                <p>Number of dogs fetched: {data.length}</p>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Picture</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data.map(breed => (
+                        <tr key={breed.id}>
+                            <td>{breed.name}</td>
+                            <td>
+                                <img src={breed.image.url} alt={breed.name} height={50}/>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default App
